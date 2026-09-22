02c1561f7e18558a71c435cfd035a0a1ff8443d1
Ioannis Mavrodimos <support@iluma.gr>
2026-09-22 18:34:28 +0300

    refactor: group homepage sections in pairs

--- front-page.php
+++ front-page.php
@@ -16,17 +16,20 @@ get_header();
 
     <?php get_template_part('template-parts/home/hero'); ?>
 
-    <?php get_template_part('template-parts/home/product-categories'); ?>
+    <div class="home-section-group home-section-group--one">
+        <?php get_template_part('template-parts/home/product-categories'); ?>
+        <?php get_template_part('template-parts/home/product-finder-preview'); ?>
+    </div>
 
-    <?php get_template_part('template-parts/home/product-finder-preview'); ?>
+    <div class="home-section-group home-section-group--two">
+        <?php get_template_part('template-parts/home/why-spek'); ?>
+        <?php get_template_part('template-parts/home/catalogue-preview'); ?>
+    </div>
 
-    <?php get_template_part('template-parts/home/why-spek'); ?>
-
-    <?php get_template_part('template-parts/home/catalogue-preview'); ?>
-
-    <?php get_template_part('template-parts/home/store-locator-preview'); ?>
-
-    <?php get_template_part('template-parts/home/b2b-cta'); ?>
+    <div class="home-section-group home-section-group--three">
+        <?php get_template_part('template-parts/home/store-locator-preview'); ?>
+        <?php get_template_part('template-parts/home/b2b-cta'); ?>
+    </div>
 
 </main>
 
