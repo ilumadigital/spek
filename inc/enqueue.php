74b5c351d76909394e95b6907f68039e8921c424
Ioannis Mavrodimos <support@iluma.gr>
2026-09-24 16:52:29 +0300

    Load homepage client revision styles

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -20,6 +20,15 @@ function spek_enqueue_assets(): void
 
     wp_enqueue_style('spek-multilingual', SPEK_THEME_URI . '/assets/css/multilingual.css', ['spek-main'], filemtime(SPEK_THEME_DIR . '/assets/css/multilingual.css'));
 
+    if (is_front_page() || (function_exists('spek_is_english_home_request') && spek_is_english_home_request())) {
+        wp_enqueue_style(
+            'spek-home-client-refresh',
+            SPEK_THEME_URI . '/assets/css/home-client-refresh.css',
+            ['spek-main', 'spek-multilingual'],
+            filemtime(SPEK_THEME_DIR . '/assets/css/home-client-refresh.css')
+        );
+    }
+
     wp_enqueue_script(
         'spek-main',
         SPEK_THEME_URI . '/assets/js/main.js',
