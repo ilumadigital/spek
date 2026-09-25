e9632f94c4640421c271953854fbe4ea322a393b
Ioannis Mavrodimos <support@iluma.gr>
2026-09-25 18:01:28 +0300

    Match company Why SPEK wrapper to homepage

--- templates/template-company.php
+++ templates/template-company.php
@@ -15,7 +15,9 @@ get_header();
 <main id="main" class="site-main page-company spek-company-page">
     <?php get_template_part('template-parts/company/company-hero'); ?>
     <?php get_template_part('template-parts/company/history'); ?>
-    <?php get_template_part('template-parts/home/why-spek'); ?>
+    <div class="home-section-group home-section-group--two company-why-spek-group">
+        <?php get_template_part('template-parts/home/why-spek'); ?>
+    </div>
     <?php get_template_part('template-parts/company/values'); ?>
     <?php get_template_part('template-parts/company/certifications'); ?>
 </main>
