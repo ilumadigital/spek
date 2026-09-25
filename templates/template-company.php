ac9424759159c2116616907d957afcb2f312caf9
Ioannis Mavrodimos <support@iluma.gr>
2026-09-25 17:55:51 +0300

    Replace company production section with Why SPEK

--- templates/template-company.php
+++ templates/template-company.php
@@ -15,7 +15,7 @@ get_header();
 <main id="main" class="site-main page-company spek-company-page">
     <?php get_template_part('template-parts/company/company-hero'); ?>
     <?php get_template_part('template-parts/company/history'); ?>
-    <?php get_template_part('template-parts/company/production'); ?>
+    <?php get_template_part('template-parts/home/why-spek'); ?>
     <?php get_template_part('template-parts/company/values'); ?>
     <?php get_template_part('template-parts/company/certifications'); ?>
 </main>
